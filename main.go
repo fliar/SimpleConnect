0eee1ffdbc0caab545e91ba9ba516a9741b5ec73
fliar <fliar@live.jp>
2015-11-05 15:01:58 +0800

    basic work

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 import(
 	"fmt"
+	"net"
+	"flag"
 )
+
+const NET_PROTOCOL 			= "tcp"
+const SERVER_IP				= "192.168.202.78"
+const PRIMARY_PORT			= ":8013"
+const SERVER_PRIMARY_ADDR	= SERVER_IP + PRIMARY_PORT
 func main(){
-	fmt.Println("Simple Connect")
+	remoteAddr := flag.String("r", "127.0.0.1:1234", "remote address")
+	flag.Parse()
+	conn, err := net.Dial(NET_PROTOCOL, *remoteAddr)
+	if err != nil {
+		panic(err)
+	}
+	localAddr	:= conn.LocalAddr().String()
+	conn.Close()
+	fmt.Println(localAddr)
 }
