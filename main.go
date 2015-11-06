65fd3a84d5daa31730c63021e65cad68196c7714
fliar <fliar@live.jp>
2015-11-06 14:17:51 +0800

    basic function

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ const SERVER_IP				= "192.168.202.78"
 const PRIMARY_PORT			= ":8013"
 const SERVER_PRIMARY_ADDR	= SERVER_IP + PRIMARY_PORT
 func main(){
-	remoteAddr := flag.String("r", "127.0.0.1:1234", "remote address")
+	remoteAddr := flag.String("r", SERVER_PRIMARY_ADDR, "remote address")
 	flag.Parse()
 	conn, err := net.Dial(NET_PROTOCOL, *remoteAddr)
 	if err != nil {
